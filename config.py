3bf6b1c3ea401804dfd616c20f25579d85df6e67
M. Dimitrova <milena.bd@gmail.com>
2021-03-06 11:56:23 +0200

    Add configuration properties

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
+iot23_file_name_pattern = '/**/conn.log.labeled'
+iot23_malicious_type_column_name = 'detailed-label'
 iot23_dataset_location = '<replace_with_your_file_path>\iot23_small'
 iot23_output_directory = '<replace_with_your_file_path>\iot23_small\processed'
